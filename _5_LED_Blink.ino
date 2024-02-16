6e084a9d93140c4c8055d209ae0c5307a9bf4e29
Chris <55931405+cbzoozoom7@users.noreply.github.com>
2024-02-16 10:23:06 -0600

    Add files via upload

---
+++ _5_LED_Blink.ino
@@ -0,0 +1,22 @@
+int Delay = 500;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(3,OUTPUT);
+pinMode(5,OUTPUT);
+pinMode(6,OUTPUT);
+pinMode(9,OUTPUT);
+pinMode(10,OUTPUT);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+for(int a = 5;a <= 10;a++) {
+  digitalWrite(a,HIGH);
+  delay(Delay);
+  digitalWrite(a,LOW);
+}
+for(int b = 9;b >= 3;b--){
+  digitalWrite(b,HIGH);
+  delay(Delay);
+  digitalWrite(b,LOW);
+}
+}
